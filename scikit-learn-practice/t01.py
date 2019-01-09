a0ba56c4581250fe0ac32ae6084a40339cfd8577
Truman Zhou <truman.ck.zhou@gmail.com>
2019-01-09 10:30:26 +0800

    init a code

---
+++ scikit-learn-practice/t01.py
@@ -0,0 +1,10 @@
+from sklearn import tree
+#import numpy as np
+
+if __name__ == '__main__':
+    X = [[0, 0], [1, 1]]
+    Y = [0, 1]
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X, Y)
+    x = clf.predict([[2., 2.]])
+    print(x)
