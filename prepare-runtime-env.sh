e5d6288ba9eb077fef7f1298d90f902e1c009c36
Truman Zhou <trumanz.ck.zhou@gmail.com>
2019-01-09 12:06:43 +0800

    modity prepare-runtime-env.sh

--- prepare-runtime-env.sh
+++ prepare-runtime-env.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh   -b -p  $(pwd)/miniconda3
+export PATH="$(pwd)/miniconda3/bin:$PATH"
+conda update --yes  -q conda
+conda info -a
+conda create --yes  --name  testenv python=3.7  scikit-learn
+source activate testenv
